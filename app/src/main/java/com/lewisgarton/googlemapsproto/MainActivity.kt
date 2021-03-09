21c2f31ab6631278225a180f5e9fb5ba4f3c9e3c
lewis <lewisgarton@gmail.com>
2021-03-09 19:10:40 +1300

    add google places dependency and api key to secrets file

--- app/src/main/java/com/lewisgarton/googlemapsproto/MainActivity.kt
+++ app/src/main/java/com/lewisgarton/googlemapsproto/MainActivity.kt
@@ -2,10 +2,20 @@ package com.lewisgarton.googlemapsproto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.libraries.places.api.Places
+
+private const val PLACES_API_KEY = BuildConfig.API_KEY
 
 class MainActivity : AppCompatActivity() {
+
+    private val placesClient by lazy {
+        Places.initialize(applicationContext, PLACES_API_KEY)
+        Places.createClient(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
