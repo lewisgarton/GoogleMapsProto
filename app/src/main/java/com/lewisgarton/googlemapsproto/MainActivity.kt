8597b54bb7cbf7f37dc9adebe0b4557bd6e846be
lewis <lewisgarton@gmail.com>
2021-03-09 19:40:59 +1300

    add auto complete search bar

--- app/src/main/java/com/lewisgarton/googlemapsproto/MainActivity.kt
+++ app/src/main/java/com/lewisgarton/googlemapsproto/MainActivity.kt
@@ -2,9 +2,15 @@ package com.lewisgarton.googlemapsproto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.common.api.Status
 import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 
 private const val PLACES_API_KEY = BuildConfig.API_KEY
+private const val TAG = "Places API"
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +22,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        placesClient
+
+        val autocompleteFragment =
+            supportFragmentManager.findFragmentById(R.id.autocomplete_fragment)
+                    as AutocompleteSupportFragment
+
+        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME))
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: ${place.name}, ${place.id}")
+            }
+
+            override fun onError(status: Status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: $status")
+            }
+        })
 
     }
 }
