b050cf2e61c4549b691d917ec668b19bd2a8568b
lewis <lewisgarton@gmail.com>
2021-03-13 15:03:06 +1300

    add map fragment

--- app/src/main/java/com/lewisgarton/googlemapsproto/MainActivity.kt
+++ app/src/main/java/com/lewisgarton/googlemapsproto/MainActivity.kt
@@ -3,11 +3,18 @@ package com.lewisgarton.googlemapsproto
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.google.android.gms.common.api.Status
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PointOfInterest
 import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.model.Place
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import com.vmadalin.easypermissions.EasyPermissions
 
 private const val PLACES_API_KEY = BuildConfig.API_KEY
 private const val TAG = "Places API"
@@ -22,26 +29,57 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        placesClient
+
+        val mapsFragment = supportFragmentManager.findFragmentById(R.id.fragment)
+            as MapsFragment
+
+        mapsFragment.placesClient = placesClient
+
 
         val autocompleteFragment =
             supportFragmentManager.findFragmentById(R.id.autocomplete_fragment)
                     as AutocompleteSupportFragment
 
-        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME))
+
+
+        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG))
 
         // Set up a PlaceSelectionListener to handle the response.
         autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
             override fun onPlaceSelected(place: Place) {
                 // TODO: Get info about the selected place.
-                Log.i(TAG, "Place: ${place.name}, ${place.id}")
+                Log.i(TAG, "Place: ${place.latLng}, ${place.id}")
+                val newPos = place.latLng
+
+                if (newPos != null) {
+                    mapsFragment.googleMap?.moveCamera(
+                        CameraUpdateFactory.newLatLngZoom(
+                        newPos,
+                        15.0F
+                    ))
+                    mapsFragment.googleMap?.addMarker(
+                        MarkerOptions()
+                            .position(newPos)
+                            .title("${place.name}"))
+                }
             }
 
             override fun onError(status: Status) {
                 // TODO: Handle the error.
                 Log.i(TAG, "An error occurred: $status")
             }
+
         })
 
+
+
+
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        // EasyPermissions handles the request result.
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
 }
---
+++ app/src/main/java/com/lewisgarton/googlemapsproto/MapsFragment.kt
@@ -0,0 +1,82 @@
+package com.lewisgarton.googlemapsproto
+
+import androidx.fragment.app.Fragment
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PointOfInterest
+import com.google.android.libraries.places.api.net.FetchPlaceRequest
+import com.google.android.libraries.places.api.net.PlacesClient
+
+class MapsFragment : Fragment() {
+
+    var googleMap: GoogleMap? = null
+    var placesClient: PlacesClient? = null
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        this.googleMap = googleMap
+
+        //Add listener
+        googleMap?.setOnMarkerClickListener {
+            Log.i("PLACE CLICKED", "${it.position}")
+            true
+        }
+
+        googleMap?.setOnPoiClickListener {
+            if (placesClient != null) {
+                val location = it.latLng
+                val name = it.name
+                val placeID = it.placeId
+
+
+                val placeInfo = placesClient.fetchPlace(
+                        FetchPlaceRequest.newInstance(
+
+                        )
+                )
+            }
+
+
+            }
+
+        }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_maps, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+
+
+}
